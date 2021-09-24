96d0230f86740f10dbb61fe0a9bb910b5ed15bcd
Akkun <akkunlab@gmail.com>
2021-09-24 23:35:58 +0900

    Update rooms.js

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,21 +5,56 @@ var db = require('../src/server/firebase');
 /* GET room page. */
 router.get('/:roomName', async function (req, res, next) {
   const roomName = req.params.roomName;
-  const roomsRef = db.collection('rooms');
-  const snapshot = await roomsRef.where('name', '==', roomName).get();
+  const roomData = await getRoom(roomName); // Roomを取得
   
   // 指定のRoomがない時
-  if (snapshot.empty) res.render('404');
+  if (!roomData) {
+    res.render('404');
+    return;
+  }
 
   // 指定のRoomがある時
-  let data;
-  snapshot.forEach(doc => {
-    data = doc.data()
-    res.render('room', {
-      title: data.name,
-      room: JSON.stringify(data)
-    });
+  const data = roomData.data;
+  const playlist = await getPlaylist(roomData.id); // Playlistを取得
+
+  data.playlist = playlist;
+
+  res.render('room', {
+    title: data.name,
+    room: JSON.stringify(data)
   });
 });
+
+/* Roomを取得 */
+const getRoom = async roomName => {
+  let data = null;
+  const roomsRef = db.collection('rooms');
+  const snapshot = await roomsRef.where('name', '==', roomName).get();
+
+  // 指定のRoomがない時
+  if (snapshot.empty) return data;
+
+  // 指定のRoomがある時
+  snapshot.forEach(doc => {
+    data = { id: doc.id, data: doc.data() };
+  });
+
+  return data;
+}
+
+/* Playlistを取得 */
+const getPlaylist = async docId => {
+  const dataArray = [];
+  const roomsRef = db.collection('rooms').doc(docId).collection('playlist');
+  const snapshot = await roomsRef.orderBy('createdAt').get();
+
+  // 指定のPlaylistがない時
+  if (snapshot.empty) return dataArray;
+
+  // 指定のPlaylistがある時
+  snapshot.forEach(doc => dataArray.push(doc.data()));
+
+  return dataArray;
+}
 
 module.exports = router;
