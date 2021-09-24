f7b6fb25a0d5deef18b10b424fec313dc2603f94
Akkun <akkunlab@gmail.com>
2021-09-24 23:48:57 +0900

    Update socket.io

--- src/client/room/index.js
+++ src/client/room/index.js
@@ -1,19 +1,18 @@
-// import { config, user } from "./config";
-// import { events } from "./events";
-// import { objects } from "./objects";
-// import { initYT, controlYT } from "./yt";
+import { events } from "./events";
+import { objects } from "./objects";
+import { initYT, controlYT } from "./yt";
 
 /* 初期化 */
 const init = (() => {
   console.log(room); // 部屋情報
-  // objects.createPlayList(); // プレイリスト作成
-  // events.checkDevice(); // デバイス確認
-  // initYT(); // YouTube Player初期化
+  objects.createPlayList(); // プレイリスト作成
+  events.checkDevice(); // デバイス確認
+  initYT(); // YouTube Player初期化
 
   // // イベント
-  // document.getElementById('previous').addEventListener('click', events.onClickPlayerButton);
-  // document.getElementById('play_pause').addEventListener('click', events.onClickPlayerButton);
-  // document.getElementById('next').addEventListener('click', events.onClickPlayerButton);
-  // document.getElementById('mute').addEventListener('click', controlYT.setMute);
-  // document.getElementById('volume').addEventListener('click', controlYT.setVolume);
+  document.getElementById('previous').addEventListener('click', events.onClickPlayerButton);
+  document.getElementById('play_pause').addEventListener('click', events.onClickPlayerButton);
+  document.getElementById('next').addEventListener('click', events.onClickPlayerButton);
+  document.getElementById('mute').addEventListener('click', controlYT.setMute);
+  document.getElementById('volume').addEventListener('click', controlYT.setVolume);
 })();
--- src/client/room/socket.js
+++ src/client/room/socket.js
@@ -2,20 +2,19 @@ import { config } from "./config";
 import { controlYT } from "./yt";
 
 /* 基本設定 */
-// const ioTime = io(`${config.host}/io_time`);  // 時刻同期
-// const ioSet = io(`${config.host}/io_set`);    // 設定
+const socket = io(config.host);
 
 /* イベント */
 const socketEvents = {
 
     send(eventName, data) { // データ送信
-        ioSet.emit(eventName, data);
+        socket.emit(eventName, data);
         console.log('[socket] send data');
     }
 
 };
 
 /* 受信イベント */
-ioSet.on("playerButton", controlYT.onReceiveButtonEvents); // プレイヤーボタンクリック
+socket.on("playerButton", controlYT.onReceiveButtonEvents); // プレイヤーボタンクリック
 
 export { socketEvents };
