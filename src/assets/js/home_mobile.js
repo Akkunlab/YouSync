323f2aa2589c5aab8b4b8cfd44c1fb32d7301361
Akkun <akkunlab@gmail.com>
2021-09-24 12:25:10 +0900

    Add tab

---
+++ src/assets/js/home_mobile.js
@@ -0,0 +1,24 @@
+/* スクロールアニメーション */
+let offset = 0;
+const scrollAnimation = lastPosition => {
+  const header = document.getElementById('header');
+  const tab = document.getElementById('tab');
+  const height = header.clientHeight;
+
+  if (lastPosition > height) {
+    if (lastPosition > offset) {
+      header.classList.add('animation');
+      tab.classList.add('animation');
+    } else {
+      header.classList.remove('animation');
+      tab.classList.remove('animation');
+    }
+
+    offset = lastPosition;
+  }
+}
+
+/* スクロール時 */
+window.onscroll = () => {
+  scrollAnimation(window.scrollY);
+}
