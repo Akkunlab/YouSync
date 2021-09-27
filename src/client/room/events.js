737c2e22efae3e16a12e14232f2b558d26b27cca
Akkun <akkunlab@gmail.com>
2021-09-28 08:42:52 +0900

    Update events.js

--- src/client/room/events.js
+++ src/client/room/events.js
@@ -7,7 +7,6 @@ const events = {
 
   checkDevice() { // デバイスチェック
     if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android|Mobile)/i)) { // モバイル
-      document.getElementById('pop_input').blur();                              // 初期画面フォーカス外す
       document.getElementsByClassName('volume')[0].style.display = 'none';      // ボリュームバー非表示
       document.getElementsByClassName('video_time')[0].style.display = 'none';  // 動画時間非表示
       user.device = 'mobile';
