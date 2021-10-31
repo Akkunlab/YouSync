3c494cd18a736f2764cad1e9a1304abf11fe1793
Akkun <akkunlab@gmail.com>
2021-10-31 12:47:17 +0900

    Update Home style

--- src/client/index/index.js
+++ src/client/index/index.js
@@ -1,6 +1,24 @@
-import "../sass/style.scss"; // スタイルシートの読込
+import "../sass/main.scss"; // スタイルシートの読込
 
-/* 初期化 */
-const init = (() => {
-  console.log('Hello World!'); // テスト
-})();
+/* Hamburger menu */
+const navInput = document.getElementById('nav_input');
+const menu = document.getElementById('menu');
+const blocker = document.getElementById('nav_blocker');
+
+document.getElementById('nav_input').addEventListener('change', () => {
+  navInput.checked ? menu.classList.remove('hidden') : menu.classList.add('hidden');
+});
+
+function initHamburgerMenu() {
+  if (1280 < window.innerWidth) {
+    navInput.checked = true;
+    menu.classList.remove('hidden')
+    blocker.style.visibility = 'hidden';
+  } else {
+    navInput.checked = false;
+    blocker.style.visibility = 'unset';
+  }
+}
+
+initHamburgerMenu();
+window.onresize = initHamburgerMenu;
